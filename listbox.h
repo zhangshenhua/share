1477eecff8b33ffdbb0944637ce50ad2e72db680
shenhua.zhang <shenhua.zhang@hisoft.com>
2011-09-05 16:28:00 +0800

    Edited listbox.h via GitHub

--- listbox.h
+++ listbox.h
@@ -1,14 +1,44 @@
+
 #ifndef _LISTBOX_H_
 #define _LISTBOX_H_
-#include "defines"
+#include "defines.h"
+#include "hwidget.h"
+#include "hcontainer.h"
 
-classdef(ListBox)
-{
+typedef HRect Rect;
+typedef HWidget Item;
 
+classdef(VideoItem) {
+	//base
+	HContainer* p_HContainer_base;
 	
-//public:
-	void additem( ListBox* p_this, VideoItem* p_vitem);
-			
+	Rect rect_image ;
+
+	struct  
+	{
+		void (*add_item)    ( VideoItem* p_this, Item* p_item );
+		void (*remove_item)	( VideoItem* p_this, Item* p_item );  
+	}public_ops;
+
+};
+
+extern VideoItem_new();
+extern VideoItem_delete( VideoItem* p_it );
+
+
+classdef(ListBox) {
+	//base
+	HContainer* p_HContainer_base;
+
+
+	//public:
+	struct  
+	{
+		void (*add_item)    ( ListBox* p_this, Item* p_item );
+		void (*remove_item)	( ListBox* p_this, Item* p_item );  
+	}public_ops;
+	
+	
 };
 
 extern ListBox* ListBox_new( Rect rect );
